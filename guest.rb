8191d53d07f296490797f83f25050408a9a0e2d3
Nadja Kelly <nmfort-info@yahoo.de>
2018-12-02 22:13:17 +0000

    update rb files

--- guest.rb
+++ guest.rb
@@ -1,10 +1,26 @@
-# class Guest
-#
-#   def initialize(name, wallet = 0, songs_play_list = nil)
-#     @name = name
-#     @wallet = wallet
-#     @songs_play_list = songs_play_list
-#   end
-#
-#
-# end
+class Guest
+
+
+
+
+  attr_reader :name, :songs_play_list
+  attr_accessor :wallet
+
+  def initialize(name, wallet = 0, songs_play_list = nil)
+    @name = name
+    @wallet = wallet
+    @songs_play_list = songs_play_list
+  end
+
+
+  def pay_fee(pay_chosen_room)
+    @wallet -= pay_chosen_room.fee if @wallet >= pay_chosen_room.fee
+  end
+
+
+  # def
+  # end
+
+
+
+end
---
+++ room.rb
@@ -0,0 +1,31 @@
+class Room
+
+
+
+  attr_reader :name, :play_ist, :occupants, :entrance_fee
+  def initialize(name, playlist, occupants)
+    @name = name
+    @playlist = playlist
+    @occupants = occupants
+    @entrance_fee = 10
+    @till = 0
+    @max_number_of_guests = 6
+
+  end
+
+  def count_occupants()
+    @occupants.count()
+  end
+
+  def is_room_full?()
+    @ccupants.count <= @max_number_of_guests
+  end
+
+  def add_guest_to_room(additional_guest)
+    if (occupants.count < @max_number_of_guests) && (additional_guest.wallet > @entrance_fee)
+      @ccupants << additional_guest
+      @till - @entrance_fee
+    end
+  end
+
+  end
